bf77bc526e7f900d07ed7a467b86163ce3b9b0cf
ellen <ellentyyu@gmail.com>
2023-11-12 21:58:31 +0800

    feat: 取地點資料

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,24 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+    const [availablePlaces, setAvailablePlaces] = useState([]);
+
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            const { data } = await axios.get('http://localhost:3000/places');
+            setAvailablePlaces(data.places);
+        }
+        fetchPlaces();
+    }, []);
+
+    return (
+        <Places
+            title="Available Places"
+            places={availablePlaces}
+            fallbackText="No places available."
+            onSelectPlace={onSelectPlace}
+        />
+    );
 }
