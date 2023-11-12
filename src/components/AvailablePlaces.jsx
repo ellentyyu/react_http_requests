34afb96fd62d9b5d6b6badca29c2f094e97cf669
ellen <ellentyyu@gmail.com>
2023-11-12 22:22:59 +0800

    feat: 判斷取資料時顯示載入中狀態

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,11 +4,14 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [availablePlaces, setAvailablePlaces] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
+        setIsFetching(true);
         const fetchPlaces = async () => {
             const { data } = await axios.get('http://localhost:3000/places');
             setAvailablePlaces(data.places);
+            setIsFetching(false);
         }
         fetchPlaces();
     }, []);
@@ -17,6 +20,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
         <Places
             title="Available Places"
             places={availablePlaces}
+            isLoading={isFetching}
+            loadingText="Fetching places data..."
             fallbackText="No places available."
             onSelectPlace={onSelectPlace}
         />
