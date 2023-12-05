647cfa0d3258a9a10c4cbe2a4dc8ea6a1f39a7d2
ellen <ellen_yu@pixnet.tw>
2023-12-05 17:31:58 +0800

    feat: 加入錯誤元件及錯誤處理

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,20 +1,36 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [availablePlaces, setAvailablePlaces] = useState([]);
     const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
-        setIsFetching(true);
         const fetchPlaces = async () => {
-            const { data } = await axios.get('http://localhost:3000/places');
-            setAvailablePlaces(data.places);
+            setIsFetching(true);
+            try {
+                const data = await axios.get('http://localhost:3000/places');
+
+                if (data.status !== 200) {
+                    throw new Error('Failed to fetch places')
+                }
+                
+                setAvailablePlaces(data.data.places);
+            } catch(error) {
+                setError({ message: error.message || 'Could not fetch places, please try again later.'});
+            }
+            
             setIsFetching(false);
         }
         fetchPlaces();
     }, []);
+
+    if (error) {
+        return <Error title="An error occurred!" message={error.message} />
+    }
 
     return (
         <Places
