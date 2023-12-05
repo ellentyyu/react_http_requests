f62b3fb26ffd03fdcd27f5e5ebf7b0a73fd67f22
ellen <ellen_yu@pixnet.tw>
2023-12-05 18:06:47 +0800

    feat: 資料排序處理

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -17,13 +18,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
                 if (data.status !== 200) {
                     throw new Error('Failed to fetch places')
                 }
-                
-                setAvailablePlaces(data.data.places);
+
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const sortedPlaces = sortPlacesByDistance(data.data.places, position.coords.latitude, position.coords.longitude)
+                    setAvailablePlaces(sortedPlaces);
+                    setIsFetching(false);
+                });                
             } catch(error) {
                 setError({ message: error.message || 'Could not fetch places, please try again later.'});
+                setIsFetching(false);
             }
-            
-            setIsFetching(false);
         }
         fetchPlaces();
     }, []);
